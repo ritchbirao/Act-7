3a44c889c9b97582ded6dd17102331e8de56d3b0
ritchbirao <ritchbirao@gmail.com>
2024-09-03 10:26:53 +0800

    ritch

---
+++ app/Http/Controllers/Search.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Search extends Controller
+{
+    public static function search($keyword) {
+        return DB::table("user")
+        ->where("lname", $keyword)
+        ->get();
+    }
+}
